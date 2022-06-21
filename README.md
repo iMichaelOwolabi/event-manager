0d75c8fd657b7b550e06c3be5f2e3d22c5de8a14
Michael Owolabi <michael.i.owolabi@gmail.com>
2022-06-21 21:18:37 +0100

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # event-manager
 This is an event management platform built on Redis
+
+### Technology
+* Node.js - Express
+* Redis - Database
+* RediSearch - Redis module
---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT ?? 3000;
+
+app.get('', (req, res) => {
+  res.send('Welcome to the event management platform built on redis');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on: http://localhost:${port}`);
+});
